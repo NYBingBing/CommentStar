38a4c004554b77e2dc283bd4d88ac9909cc5ea9c
bingbing.luo <752307629@qq.com>
2016-06-01 13:29:42 +0800

    Initial Commit

---
+++ testStar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testStar
+//
+//  Created by bingbing.luo on 16/6/1.
+//  Copyright © 2016年 bingbing.luo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testStar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testStar
+//
+//  Created by bingbing.luo on 16/6/1.
+//  Copyright © 2016年 bingbing.luo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
